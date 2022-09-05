63ffa06e70d81ff921edd127ed683ad4dee60c41
mustapha rachidi <mustapharachidipro@gmail.com>
2022-09-05 20:18:59 +0100

    change map

--- map/insert.cpp
+++ map/insert.cpp
@@ -1,84 +1,70 @@
-#include <iostream>
-#include <map>
+		node_type*		deleteNode(node_type *root,const key_type key)
+		{
+			if (root == nullptr)
+				return nullptr;
+			else if (!comp(key,root->data.first) && (key != root->data.first))
+				root->right = deleteNode(root->right, key);
+			else if (comp(key,root->data.first))
+				root->left = deleteNode(root->left, key);
+			else if (key == root->data.first)
+			{
+				if (root->left == nullptr)
+				{
+					node_type *new_parent = root->right;
+					node_type *temp = root->rootPtr;
+					nodeAlloc.deallocate(root, 1);
+					if (new_parent != nullptr)
+						new_parent->rootPtr = temp;
+					return new_parent;
+				}
+				else if (root->right == nullptr )
+				{
+					node_type *new_parent = root->left;
+					node_type *temp = root->rootPtr;
+					nodeAlloc.deallocate(root, 1);
+					if (new_parent != nullptr)
+						new_parent->rootPtr = temp;
+					return new_parent;
+				}
+				else{
+					node_type *ret = minValue(root->left);
+					nodeAlloc.destroy(root);
+					nodeAlloc.construct(root, ft::make_pair(ret->data.first, ret->data.second));
+					root->left = deleteNode(root->left, ret->data.first);
+				}
+			}
+			root = balanceAfterRemove(root);
+			return root;
+		}
 
-int main ()
-{
-  std::map<char,int> mymap;
-
-  // first insert function version (single parameter):
-  std::pair<char,int> p;
-  p = std::make_pair('a',100) ;
-  mymap.insert ( p );
-  mymap.insert ( std::pair<char,int>('z',200) );
-
-  std::pair<std::map<char,int>::iterator,bool> ret;
-  ret = mymap.insert ( std::pair<char,int>('z',500) );
-  if (ret.second == false) 
-  {
-    std::cout << "element 'z' already existed";
-    std::cout << " with a value of " << ret.first->second << '\n';
-  }
-
-  // second insert function version (with hint position):
-  std::map<char,int>::iterator it = mymap.begin();
-  mymap.insert (it, std::pair<char,int>('b',300));  // max efficiency inserting
-  mymap.insert (it, std::pair<char,int>('c',400));  // no max efficiency inserting
-
-  // third insert function version (range insertion):
-  std::map<char,int> anothermap;
-  anothermap.insert(mymap.begin(), mymap.find('c'));
-
-  // showing contents:
-  std::cout << "mymap contains:\n";
-  for (it=mymap.begin(); it!=mymap.end(); ++it)
-    std::cout << it->first << " => " << it->second << '\n';
-
-  std::cout << "anothermap contains:\n";
-  for (it=anothermap.begin(); it!=anothermap.end(); ++it)
-    std::cout << it->first << " => " << it->second << '\n';
-
-  return 0;
-}
-
-// AVL tree implementation in C++
-
-#include <iostream>
-using namespace std;
-
-class Node {
-   public:
-  int key;
-  Node *left;
-  Node *right;
-  int height;
-};
-
-
-// Node *newNode(int key) {
-//   Node *node = new Node();
-//   node->key = key;
-//   node->left = NULL;
-//   node->right = NULL;
-//   node->height = 1;
-//   return (node);
-// }
-
-// Node *insertNode(Node *node, int key) {
-//   if (node == NULL)
-//     return (newNode(key));
-//   if (key < node->key)
-//     node->left = insertNode(node->left, key);
-//   else if (key > node->key)
-//     node->right = insertNode(node->right, key);
-//   else
-//     return node;
-//   return node;
-// }
-
-// int main() 
-// {
-//     Node *root = NULL;
-//     root = insertNode(root, 1);
-//     root = insertNode(root, 2);
-//     root = insertNode(root, 3);
-// }
+		node_type		*balanceAfterRemove(node_type *node)
+		{
+			node->Height = 1 + max(height(node->left), height(node->right));
+  			int balanceFactor = getBalanceFactor(node);
+			
+			//? Left Left Case
+			if (balanceFactor > 1 &&
+				getBalanceFactor(node->left) >= 0)
+				return leftLeftCase(node);
+		
+			//? Left Right Case
+			if (balanceFactor > 1 &&
+				getBalanceFactor(node->left) < 0)
+			{
+				node->left = rightRightCase(node->left);
+				return leftLeftCase(node);
+			}
+		
+			//? Right Right Case
+			if (balanceFactor < -1 &&
+				getBalanceFactor(node->right) <= 0)
+				return rightRightCase(node);
+		
+			//? Right Left Case
+			if (balanceFactor < -1 &&
+				getBalanceFactor(node->right) > 0){
+				node->right = leftLeftCase(node->right);
+				return rightRightCase(node);
+			}
+			return node;
+		}
